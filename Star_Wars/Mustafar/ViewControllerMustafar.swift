8829ea69771eab865e1d137b79f93ad3b83396f8
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-02-01 14:49:08 +0700

    Рефакторинг

---
+++ Star_Wars/Mustafar/ViewControllerMustafar.swift
@@ -0,0 +1,18 @@
+//
+//  ViewControllerMustafar.swift
+//  Star_Wars
+//
+//  Created by Елена on 01.02.2023.
+//
+
+import UIKit
+
+class ViewControllerMustafar: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
